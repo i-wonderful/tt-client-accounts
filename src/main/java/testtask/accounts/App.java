d50d83ccc6c664d8bad4c26e5ca6d318d49616ff
Strannica <Strannica@Strannica-THINK>
2018-01-23 17:27:14 +0300

    First

---
+++ src/main/java/testtask/accounts/App.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testtask.accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Strannica
+ */
+@Controller
+@EnableAutoConfiguration
+public class App {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello!!! My Friend!!!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
