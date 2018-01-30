d2a589e0208f1e8656d03df04b5b90cfa15111c1
Волобуев Александр Викторович <volobuev_av@sptnk.co>
2018-01-30 19:09:47 +0300

    swagger test failing fixed .. spring profiles added

--- accounts/src/main/java/testtask/accounts/swagger/SwaggerConfig.java
+++ accounts/src/main/java/testtask/accounts/swagger/SwaggerConfig.java
@@ -3,6 +3,7 @@ package testtask.accounts.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -19,6 +20,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@Profile(value = "prod")
 public class SwaggerConfig {
     @Bean
     public Docket api() {
